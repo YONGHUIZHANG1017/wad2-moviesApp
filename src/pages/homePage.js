612fef98bfda3dcd880c9f856fed6e82a9fdbf47
Yonghui Zhang <68320372+maxcheung5259@users.noreply.github.com>
2020-10-23 23:37:53 +0800

    Refactor: Template list page; HTTP API calls centralized

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,50 +1,32 @@
-import React, {useState, useEffect } from "react";
-import Header from "../components/headerMovieList";
-import MovieList from "../components/movieList";
-import FilterControls from "../components/filterControls";
+import React, { useState, useEffect } from "react";
 import StubAPI from "../api/stubAPI";
+import PageTemplate from '../components/templateMovieListPage'
+import { getMovies } from "../api/tmdb-api";
 const MovieListPage = () => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => {
-        console.log(json)
-        return json.results
-      })
-      .then(movies => {
-        setMovies(movies);
-      });
+    getMovies().then(movies => {
+      setMovies(movies);
+    });
   }, []);
-  return (
-    <>
-      <Header numMovies={movies.length} />
-      <FilterControls />
-      <MovieList movies={movies} />
-    </>);
-    const addToFavorites = movieId => {
-      // Find index position of selected movie in the list
-      const index = movies.map(m => m.id).indexOf(movieId)
-  
-      StubAPI.add(movies[index])
-      const updatedList = [...movies]  // Make a copy of the movie list
-      updatedList.splice(index, 1)     // Remove selected movie from home page list
-      setMovies(updatedList)  
-    }
-  
-    return (
-      <>
-        <Header numMovies={displayedMovies.length} />
-        <FilterControls onUserInput={handleChange} />
-        <MovieList
-          movies={displayedMovies}
-          buttonHandler={addToFavorites}
-        />     {/* CHANGED */}
-      </>
-    );
 
-    }
+  const addToFavorites = movieId => {
+    setMovies(movies => {
+      const index = movies.map(m => m.id).indexOf(movieId);
+      StubAPI.add(movies[index]);
+      let newMoviesState = [...movies]
+      newMoviesState.splice(index, 1);
+      return newMoviesState;
+    });
+  };
+
+  return (
+      <PageTemplate
+        title='Discover Movies'
+        movies={movies}
+        buttonHandler={addToFavorites}
+      />
+  );
+};
 
 export default MovieListPage;
