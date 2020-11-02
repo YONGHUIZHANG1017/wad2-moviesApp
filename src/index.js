ff7fceaf790b547963a4999f85ba63a81a710f39
Yonghui Zhang <68320372+maxcheung5259@users.noreply.github.com>
2020-11-03 00:08:51 +0800

    Full review page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'   
 import FavoriteMoviesPage from './pages/favoritesMoviesPage'       // NEW
+import MovieReviewPage from "./pages/movieReviewPage";
 const App = () => {
   return (
     <BrowserRouter>
@@ -23,11 +24,13 @@ const App = () => {
           </li>
         </ul>
         <Switch>
+        <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
           <Route path="/" component={HomePage} />
           <Redirect from="*" to="/" />
         </Switch>
+     
       </div>
     </div>
   </BrowserRouter>
