92b0a68798a33a8704042472203d74b00b1e2b75
Yonghui Zhang <68320372+maxcheung5259@users.noreply.github.com>
2020-11-04 14:50:26 +0800

    E2E tests - Navigation - Movies Details page

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -48,5 +48,19 @@ describe("Navigation", () => {
       cy.get("h2").contains("No. Movies");
     });
   });
- 
+   describe("From the Movie Details page ", () => {
+        beforeEach(() => {
+          cy.visit(`/movies/${movieId}`);
+        });
+        it("should change browser URL when show/hide reviews is clicked", () => {
+          cy.contains("Show Reviews").click();
+          cy.url().should("include", `/movies/${movieId}/reviews`);
+          cy.contains("Hide Reviews").click();
+          cy.url().should("not.include", `/movies/${movieId}/reviews`);
+        });
+        it("navigate to the full review page when a 'Full Review' link is clicked", () => {
+          cy.contains("Full Review").click();
+          cy.url().should("include",`/reviews/${movieReview||movieReview2}`);
+        });
+      });
 });
