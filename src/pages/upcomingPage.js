f176a6d838519d38efed17660c1634d28254f487
Yonghui Zhang <68320372+maxcheung5259@users.noreply.github.com>
2020-11-30 17:54:28 +0800

    New button for upcoming movies page

--- src/pages/upcomingPage.js
+++ src/pages/upcomingPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavoritesButton from '../components/buttons/addToFavorites'
+import AddToWatchListButton from '../components/buttons/addToWatchList'
 const MovieListPage = () => {
   const [movies, setMovies] = useState([]);
   
@@ -18,7 +18,7 @@ const MovieListPage = () => {
         title='Upcoming Movies'
         movies={movies}
         action={(movie) => {
-          return <AddToFavoritesButton movie={movie} /> 
+          return <AddToWatchListButton movie={movie} /> 
         }}
       
       />
