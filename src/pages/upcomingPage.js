e7a14dd87698a5be3df12576d0a76fec87eb6379
Yonghui Zhang <68320372+maxcheung5259@users.noreply.github.com>
2020-11-30 17:38:37 +0800

    Fix upcoming movies page

--- src/pages/upcomingPage.js
+++ src/pages/upcomingPage.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import StubAPI from "../api/stubAPI";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
-
+import AddToFavoritesButton from '../components/buttons/addToFavorites'
 const MovieListPage = () => {
   const [movies, setMovies] = useState([]);
   
@@ -12,21 +11,16 @@ const MovieListPage = () => {
     });
   }, []);
 
-  const addToFavorites = movieId => {
-    setMovies(movies => {
-      const index = movies.map(m => m.id).indexOf(movieId);
-      StubAPI.add(movies[index]);
-      let newMoviesState = [...movies]
-      newMoviesState.splice(index, 1);
-      return newMoviesState;
-    });
-  };
+
 
   return (
       <PageTemplate
         title='Upcoming Movies'
         movies={movies}
-        buttonHandler={addToFavorites}
+        action={(movie) => {
+          return <AddToFavoritesButton movie={movie} /> 
+        }}
+      
       />
   );
 };
